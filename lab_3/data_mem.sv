17d61ec4b94dd719f8dc42e8bb2f05589c2ba729
Vuzhos <31344427+Vuzhos@users.noreply.github.com>
2023-10-16 15:55:37 +0300

    Create data_mem.sv

---
+++ lab_3/data_mem.sv
@@ -0,0 +1,30 @@
+module data_mem(
+  input  logic        clk_i,
+  input  logic        mem_req_i,
+  input  logic        write_enable_i,
+  input  logic [31:0] addr_i,
+  input  logic [31:0] write_data_i,
+  output logic [31:0] read_data_o
+);
+
+    logic [31:0] memory [4095:0];
+
+    always_ff @(posedge clk_i) begin
+        if (!mem_req_i)
+            read_data_o = 32'hfa11_1eaf;
+        else if (!write_enable_i) begin
+            if (addr_i > 16383)
+                read_data_o = 32'hdead_beef;
+            else
+                read_data_o <= memory[addr_i >>> 2];
+        end
+         else begin
+            if (addr_i > 16383)
+                read_data_o = 32'hdead_beef;
+            else
+                read_data_o = 32'hfa11_1eaf;
+                memory[addr_i >>> 2] <= write_data_i;
+         end
+    end 
+ 
+endmodule
