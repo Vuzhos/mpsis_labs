6f5e2ba09b87c8f8453fae8068f59200f5da5e43
Vuzhos <31344427+Vuzhos@users.noreply.github.com>
2023-10-16 15:55:03 +0300

    Create instr_mem.sv

---
+++ lab_3/instr_mem.sv
@@ -0,0 +1,14 @@
+module instr_mem(
+  input  logic [31:0] addr_i,
+  output logic [31:0] read_data_o
+);
+    logic [31:0] memory [1023:0];
+    initial $readmemh("program.txt", memory);
+    
+    always @ (addr_i) begin
+        if (addr_i > 4095)
+            read_data_o = 0;
+        else
+            read_data_o = memory[addr_i >>> 2];
+    end
+endmodule
