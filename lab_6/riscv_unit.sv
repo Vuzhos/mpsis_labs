7e4653c94518fe463ffc6e977c671ace604355c0
Vuzhos <31344427+Vuzhos@users.noreply.github.com>
2023-11-14 22:45:11 +0300

    Update riscv_unit.sv

--- lab_6/riscv_unit.sv
+++ lab_6/riscv_unit.sv
@@ -3,23 +3,22 @@ module riscv_unit(
   input  logic        rst_i
 );
     
-    logic [31:0] RA_instr;
-    logic [31:0] RD_instr;
-    logic [31:0] RD_memory;
-    logic [31:0] WD_memory;
-    logic [31:0] RA_memory;
+    logic [31:0] instr_addr;
+    logic [31:0] instr;
+    logic stall;
+    logic new_stall;    
     
+    logic [31:0] memory_addr;
+    logic [31:0] memory_rd;
+    logic [31:0] memory_wd;
     logic mem_req;
     logic mem_we;
     
-    logic stall;
-    logic new_stall;
-    
     assign new_stall = !stall && mem_req;
     
-    instr_mem inst_memory(
-        .addr_i(RA_instr),
-        .read_data_o(RD_instr)
+    instr_mem instr_memory(
+        .addr_i(instr_addr),
+        .read_data_o(instr)
     );
     
     riscv_core core(
@@ -27,30 +26,29 @@ module riscv_unit(
         .rst_i(rst_i),
 
         .stall_i(new_stall),
-        .instr_i(RD_instr),
-        .mem_rd_i(RD_memory),
+        .instr_i(instr),
+        .mem_rd_i(memory_rd),
 
-        .instr_addr_o(RA_instr),
-        .mem_addr_o(RA_memory),
+        .instr_addr_o(instr_addr),
+        .mem_addr_o(memory_addr),
         .mem_size_o(),
         .mem_req_o(mem_req),
         .mem_we_o(mem_we),
-        .mem_wd_o(WD_memory)
+        .mem_wd_o(memory_wd)
     );
     
     data_mem data_memory(
         .clk_i(clk_i),
         .mem_req_i(mem_req),
         .write_enable_i(mem_we),
-        .addr_i(RA_memory),
-        .write_data_i(WD_memory),
-        .read_data_o(RD_memory)
+        .addr_i(memory_addr),
+        .write_data_i(memo),
+        .read_data_o(memory_rd)
     );
     
     always_ff @ (posedge clk_i) begin
-      if (rst_i) stall <= 1;
-      else stall = new_stall;
+      if (rst_i) stall <= 0;
+      else stall <= new_stall;
     end 
-    
-    
+
 endmodule
