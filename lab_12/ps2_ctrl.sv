b8129421caad0b42b44c4f7140d964a4f1204f4e
Vuzhos <31344427+Vuzhos@users.noreply.github.com>
2023-12-24 02:47:45 +0300

    Fixed ps2_ctrl.sv

--- lab_12/ps2_ctrl.sv
+++ lab_12/ps2_ctrl.sv
@@ -1,75 +1,87 @@
 module ps2_sb_ctrl(
-/*
-    Часть интерфейса модуля, отвечающая за подключение к системной шине
-*/
-    input  logic         clk_i,
-    input  logic         rst_i,
-    input  logic [31:0]  addr_i,
-    input  logic         req_i,
-    input  logic [31:0]  write_data_i,
-    input  logic         write_enable_i,
-    output logic [31:0]  read_data_o,
+  input  logic         clk_i,
+  input  logic         rst_i,
+  input  logic [31:0]  addr_i,
+  input  logic         req_i,
+  input  logic [31:0]  write_data_i,
+  input  logic         write_enable_i,
+  output logic [31:0]  read_data_o,
 
-/*
-    Часть интерфейса модуля, отвечающая за отправку запросов на прерывание
-    процессорного ядра
-*/
+  output logic        interrupt_request_o,
+  input  logic        interrupt_return_i,
 
-    output logic        interrupt_request_o,
-    input  logic        interrupt_return_i,
-
-/*
-    Часть интерфейса модуля, отвечающая за подключение к модулю,
-    осуществляющему прием данных с клавиатуры
-*/
-    input  logic kclk_i,
-    input  logic kdata_i
+  input  logic kclk_i,
+  input  logic kdata_i
 );
-
     logic [7:0] scan_code;
     logic       scan_code_is_unread;
-    
-    logic keycode_valid;
     logic [7:0] keycode;
-    
-    PS2Receiver ps2(
-        .clk_i (clk_i),
-        .rst_i (rst_i),
-        .kclk_i (kclk_i),
-        .kdata_i (kdata_i),
-        .keycodeout_o (keycode),
-        .keycode_valid_o (keycode_valid)
+    logic       keycode_is_valid;
+
+    PS2Receiver PS2_Receiver_inst (
+        .clk_i(clk_i),
+        .rst_i(rst_i),
+        .kclk_i(kclk_i),
+        .kdata_i(kdata_i),
+        .keycodeout_o(keycode),
+        .keycode_valid_o(keycode_is_valid)
     );
-      
-    always_ff @ (posedge clk_i) begin
+    
+    assign interrupt_request_o = scan_code_is_unread;
+
+    always_ff @(posedge clk_i) begin
         if(rst_i) begin
             scan_code <= 0;
             scan_code_is_unread <= 0;
-        end
-        else begin
-            if (keycode_valid) begin
-                scan_code <= keycode;
-                scan_code_is_unread <= 1;
-            end
-            else if (interrupt_return_i) scan_code_is_unread <= 0;
-        
-            if (req_i) begin
-                if (write_enable_i) begin
-                    if (addr_i == 32'h24) begin
-                        scan_code <= 0;
-                        scan_code_is_unread <= 0;
-                    end
+        end else begin
+            case(keycode_is_valid)
+                1: begin
+                    scan_code <= keycode;
+                    scan_code_is_unread <= 1;
                 end
-                else begin
-                    if (addr_i == 0) begin
-                        read_data_o <= {{24{1'b0}}, scan_code};
-                        if (!keycode_valid) scan_code_is_unread <= 0;
+                0: begin
+                    if(interrupt_return_i) scan_code_is_unread <= 0;
+                    if(req_i && !write_enable_i) begin
+                        case(addr_i)
+                            0: scan_code_is_unread <= 0;
+                            4: scan_code_is_unread <= scan_code_is_unread;
+                            default: scan_code_is_unread <= scan_code_is_unread;
+                        endcase
+                    end 
+                    else if(req_i && write_enable_i) begin
+                        case(addr_i)
+                            24: begin
+                                if((write_data_i < 2) && write_data_i == 1) begin
+                                    scan_code <= 0;
+                                    scan_code_is_unread <= 0;
+                                end else begin
+                                    scan_code <= scan_code;
+                                    scan_code_is_unread <= scan_code_is_unread;
+                                end
+                            end
+                            default: begin
+                                scan_code <= scan_code;
+                                scan_code_is_unread <= scan_code_is_unread;
+                            end
+                        endcase
                     end
-                    else if (addr_i == 32'h4) read_data_o <= {{31{1'b0}}, scan_code_is_unread};
-                end
-            end
+                end 
+            endcase
         end
     end
-    
-    assign interrupt_request_o = scan_code_is_unread;
+
+
+    always_comb begin
+        if(rst_i) read_data_o = 0;
+        else begin
+            if(req_i && !write_enable_i) begin
+                case(addr_i)
+                    0: read_data_o = {24'b0, scan_code};
+                    4: read_data_o = {31'b0, scan_code_is_unread};
+                    default: read_data_o = read_data_o;
+                endcase
+            end 
+        end
+    end
+
 endmodule
